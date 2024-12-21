fcdb155852bfbcc5b4737032887bcfdb1b66ffb5
krajat03 <rajatunsar@gmail.com>
2024-12-21 15:41:25 +0530

    Multiple Inheritance for Employee Details

--- Inheritance_Multiple.cpp
+++ Inheritance_Multiple.cpp
@@ -1,55 +1,55 @@
 #include <iostream>
 using namespace std;
-class Enter
+// We are going to use Multiple Inheritance in our program 
+class A
 {
-public:
-    float a, b;
-    virtual void input()
-    {
+protected:
+    string name;
+    int id;
 
-        cout << "Enter two values: ";
-        cin >> a >> b;
+public:
+    void input()
+    {
+        cout << "Enter Your name: ";
+        getline(cin, name);
+        cout << "Enter Your Id number: ";
+        cin >> id;
     }
 };
-class Add : public virtual Enter
+class B
 {
+protected:
+    int salary;
+    int exp;
+
 public:
-    void sum()
+    void get()
     {
-        cout << "Sum is: " << a + b << endl;
+        cout << "Enter Your salary: ";
+        cin >> salary;
+        cout << "Enter Your experience: ";
+        cin >> exp;
     }
 };
-class Sub : public Add
+class C : public A, public B   // Here we inherit class C with parent class A and class B
 {
 public:
-    void sub()
+    void output()
     {
-        cout << "Substraction is: " << a - b << endl;
-    }
-};
-class Mul : public Sub
-{
-public:
-    void multiply()
-    {
-        cout << "Multiplication is: " << a * b << endl;
-    }
-};
-class Division : public Mul
-{
-public:
-    void division()
-    {
-        cout << "Division is: " << a / b << endl;
+        cout << endl << "Name: " << name << endl;
+        cout << "Id Number : " << id << endl;
+        cout << "Salary : " << salary << endl;
+        cout << "Experience : " << exp << endl;
     }
 };
 int main()
 {
-    Division obj;
-    obj.input();
-    obj.sum();
-    obj.sub();
-    obj.multiply();
-    obj.division();
+
+    // Class C is inherited with Class A and Class B --> Multiple inheritance
+    C Employee1;
+    Employee1.input();
+    Employee1.get();
+    Employee1.output();
+
     return 0;
-}   
+}
